5fb17a41291727714199a3e9f504e8eaeeba5740
Dima <technodimka@gmail.com>
2023-04-27 19:18:11 +0400

    Upping versionName to 1.07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 33
         versionCode = 6
-        versionName = "1.06"
+        versionName = "1.07"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue("bool", "FIREBASE_ANALYTICS_DEACTIVATED", "true")
--- app/src/main/java/org/zotero/android/database/Database.kt
+++ app/src/main/java/org/zotero/android/database/Database.kt
@@ -7,7 +7,7 @@ import java.io.File
 
 class Database {
     companion object {
-        private const val schemaVersion = 41L
+        private const val schemaVersion = 42L
 
         fun mainConfiguration(dbFile: File, context: Context): RealmConfiguration {
             Realm.init(context)
