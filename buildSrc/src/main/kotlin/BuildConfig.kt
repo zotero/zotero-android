d7d6c026fda237a1207a67c7b7e3e3fbe54b33f8
Dima <technodimka@gmail.com>
2025-04-01 20:19:05 +0900

    WebSocket connection isn't reopened on device wake. Closes #233 We now explicitly re-establish connection if user were to return to the app and there was no connection at this moment. Also fixes a few bugs in WebSocketController
    Upping versionCode to 154

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val compileSdkVersion = 34
     const val targetSdk = 34
 
-    val versionCode = 153 // Must be updated on every build
+    val versionCode = 154 // Must be updated on every build
     val version = Version(
         major = 1,
         minor = 0,
