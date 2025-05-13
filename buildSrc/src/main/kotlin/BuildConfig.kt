608dc6e4a8c9ce9ced265254aaab2f4595445b05
Dima <technodimka@gmail.com>
2025-05-13 14:13:36 +0900

    Fixes "When duplicating an Item of any type initially separate first and second author name fields get joined into a single field." Closes #242
    Upping versionCode to 167

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val compileSdkVersion = 34
     const val targetSdk = 34
 
-    val versionCode = 166 // Must be updated on every build
+    val versionCode = 167 // Must be updated on every build
     val version = Version(
         major = 1,
         minor = 0,
