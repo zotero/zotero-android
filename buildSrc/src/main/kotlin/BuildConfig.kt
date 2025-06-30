3bfab2de258b860cde3d43e6e20893c2c4526262
Dima <technodimka@gmail.com>
2025-06-30 11:53:10 +0900

    Fix for "Unable to open web links from app". Closes #249
    Upping versionCode to 186

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val compileSdkVersion = 34
     const val targetSdk = 34
 
-    val versionCode = 185 // Must be updated on every build
+    val versionCode = 186 // Must be updated on every build
     val version = Version(
         major = 1,
         minor = 0,
