11ae1e5742b367349a2a3d067dce24116bede32d
Dima <technodimka@gmail.com>
2025-11-07 20:19:37 +0900

    Updating typography #247
    Upping versionCode to 200

--- app/src/main/java/org/zotero/android/uicomponents/themem3/TypeM3.kt
+++ app/src/main/java/org/zotero/android/uicomponents/themem3/TypeM3.kt
@@ -6,20 +6,31 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val AppTypographyM3 = Typography(
-	titleLarge = TextStyle(
+    titleLarge = TextStyle(
         fontSize = 20.sp,
+        letterSpacing = 0.sp,
+        lineHeight = 28.sp,
     ),
     titleSmall = TextStyle(
+        fontSize = 14.sp,
         fontWeight = FontWeight.SemiBold,
+        letterSpacing = 0.1.sp,
+        lineHeight = 20.sp,
     ),
-	titleSmallEmphasized = TextStyle(
-		fontWeight = FontWeight.Bold,
-	),
-	bodyLarge = TextStyle(
+    titleSmallEmphasized = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.1.sp,
+        lineHeight = 20.sp,
+    ),
+    bodyLarge = TextStyle(
         fontSize = 16.sp,
         letterSpacing = 0.15.sp,
+        lineHeight = 24.sp,
     ),
-	bodyMedium = TextStyle(
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
         letterSpacing = 0.1.sp,
+        lineHeight = 20.sp,
     ),
 )
