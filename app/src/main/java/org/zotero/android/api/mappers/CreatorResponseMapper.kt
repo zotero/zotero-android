3559cb0e5547931c6e66d32037b21b57f3562af1
Dima <technodimka@gmail.com>
2025-11-26 17:42:06 +0900

    Fix for com.google.gson.JsonElement.getAsString(JsonElement.java:179) due to java.lang.UnsupportedOperationException: JsonNull which happens JSON field firstName sometimes being neither a string nor a kotlin null, but instead JsonNull.

--- app/src/main/java/org/zotero/android/api/mappers/CreatorResponseMapper.kt
+++ app/src/main/java/org/zotero/android/api/mappers/CreatorResponseMapper.kt
@@ -1,5 +1,7 @@
 package org.zotero.android.api.mappers
 
+import com.google.gson.JsonElement
+import com.google.gson.JsonNull
 import com.google.gson.JsonObject
 import org.zotero.android.api.pojo.sync.CreatorResponse
 import javax.inject.Inject
@@ -8,15 +10,26 @@ class CreatorResponseMapper @Inject constructor() {
 
     fun fromJson(json: JsonObject): CreatorResponse {
         val creatorType = json["creatorType"].asString
-        val firstName = json["firstName"]?.asString
-        val lastName = json["lastName"]?.asString
-        val name = json["name"]?.asString
+        val firstName = stringOrNull(json["firstName"])
+        val lastName = stringOrNull(json["lastName"])
+        val name = stringOrNull(json["name"])
         return CreatorResponse(
             creatorType = creatorType,
             firstName = firstName,
             lastName = lastName,
             name = name
         )
+
+    }
+
+    private fun stringOrNull(objectS: JsonElement?): String? {
+        if (objectS == null) {
+            return null
+        }
+        if (objectS is JsonNull) {
+            return null
+        }
+        return objectS.asString
 
     }
 }
